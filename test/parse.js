c7d9f695622e4e5f720f625eb0ac1f6ff864d7db
Ger Hobbelt <ger@hobbelt.com>
2017-07-12 01:34:38 +0200

    - add preliminary '`' backquote delimited multiline string support similar to ES2017 template strings (without the template variable expansion!) - fix test cases for NodeJS 8.x: octal is obsoleted and the old noctal behaviour as assumed in the tests isn't supported as a consequence.

--- test/parse.js
+++ test/parse.js
@@ -53,18 +53,18 @@ var testParseJSON5 = function (filePath, str) {
     }
     assert(err, 'Expected JSON5 parsing to fail.');
     if (errorSpec) {
-        describe("Error fixture " + filePath, function () {
-        Object.keys(errorSpec).forEach(function (key) {
-            if (key === 'message') {
-                it('Expected error message\n' + err.message + '\nto start with ' + errorSpec.message, function () {
-                    assert(err.message.indexOf(errorSpec.message) === 0);
-                });
-            } else {
-                it('Expected parse error field ' + key + ' to hold value ' + errorSpec[key], function () {
-                    assert.equal(err[key], errorSpec[key]);
-                });
-            }
-        })
+        describe('Error fixture ' + filePath.replace(/^.*parse-cases/, '...'), function () {
+            Object.keys(errorSpec).forEach(function (key) {
+                if (key === 'message') {
+                    it('Expected error message\n' + err.message + '\nto start with ' + errorSpec.message, function () {
+                        assert(err.message.indexOf(errorSpec.message) === 0);
+                    });
+                } else {
+                    it('Expected parse error field "' + key + '" to hold value "' + errorSpec[key] + '"', function () {
+                        assert.equal(err[key], errorSpec[key], "fixture info should match up");
+                    });
+                }
+            })
         });
     }
 };
@@ -73,49 +73,59 @@ function createTest(fileName, dir) {
     var ext = Path.extname(fileName);
     var filePath = Path.join(dirsPath, dir, fileName);
     var str = FS.readFileSync(filePath, 'utf8');
+    var refFilePath = Path.join(dirsPath, dir, fileName + '-ref');
+    var refStr = str;
+    if (FS.existsSync(refFilePath)) {
+        refStr = FS.readFileSync(refFilePath, 'utf8');
+    }
 
-    function parseJSON5() {
+    function parseJSON5(str) {
         return JSON5.parse(str);
     }
 
-    function parseJSON() {
+    function parseJSON(str) {
         return JSON.parse(str);
     }
 
-    function parseES5() {
+    function parseES5(str) {
         return eval('"use strict"; (\n' + str + '\n)');
     }
 
     exports[dir][fileName] = function test() {
         switch (ext) {
             case '.json':
-                assert.deepEqual(parseJSON5(), parseJSON(),
+                assert.deepEqual(parseJSON5(str), parseJSON(refStr),
                     'Expected parsed JSON5 to equal parsed JSON.');
                 break;
             case '.json5':
-                assert.throws(parseJSON,        // test validation
-                    'Test case bug: expected JSON parsing to fail.');
+                assert.throws(function () {
+                    return parseJSON(str);        // test validation
+                }, 'Test case bug: expected JSON parsing to fail.');
                 // Need special case for NaN as NaN != NaN
                 if ( fileName === 'nan.json5' ) {
-                  assert.equal( isNaN( parseJSON5() ), isNaN( parseES5() ),
-                    'Expected parsed JSON5 to equal parsed ES5.');
+                    assert.equal( isNaN( parseJSON5(str) ), isNaN( parseES5(refStr) ),
+                        'Expected parsed JSON5 to equal parsed ES5.');
                 }
                 else {
-                  assert.deepEqual( parseJSON5(), parseES5(),
-                    'Expected parsed JSON5 to equal parsed ES5.');
+                    assert.deepEqual( parseJSON5(str), parseES5(refStr),
+                        'Expected parsed JSON5 to equal parsed ES5.');
                 }
                 break;
             case '.js':
-                assert.throws(parseJSON,        // test validation
-                    'Test case bug: expected JSON parsing to fail.');
-                assert.doesNotThrow(parseES5,   // test validation
-                    'Test case bug: expected ES5 parsing not to fail.');
-                assert.throws(parseJSON5,
-                    'Expected JSON5 parsing to fail.');
+                assert.throws(function () {
+                    return parseJSON(str);        // test validation
+                }, 'Test case bug: expected JSON parsing to fail.');
+                assert.doesNotThrow(function () {
+                    return parseES5(str);      // test validation
+                }, 'Test case bug: expected ES5 parsing not to fail.');
+                assert.throws(function () {
+                    return parseJSON5(str);
+                }, 'Expected JSON5 parsing to fail.');
                 break;
             case '.txt':
-                assert.throws(parseES5,         // test validation
-                    'Test case bug: expected ES5 parsing to fail.');
+                assert.throws(function () {
+                    return parseES5(str);         // test validation
+                }, 'Test case bug: expected ES5 parsing to fail.');
                 testParseJSON5(filePath, str);
                 break;
         }
