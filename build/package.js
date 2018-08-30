67f258e8e1e6e1c1087bf790919f103e5ce7f2d8
Ger Hobbelt <ger@hobbelt.com>
2018-08-30 17:45:15 +0200

    update build/package.js script to include version updates in the README.md file

--- build/package.js
+++ build/package.js
@@ -9,3 +9,12 @@ let pkg5 = '// This is a generated file. Do not edit.\n'
 pkg5 += pkg5 = JSON5.stringify(pkg, null, 2)
 
 fs.writeFileSync(path.resolve(__dirname, '..', 'package.json5'), pkg5)
+
+
+// Also update version refs in README:
+let md = fs.readFileSync(path.resolve(__dirname, '..', 'README.md'), 'utf8');
+
+md = md
+.replace(/(@gerhobbelt\/json5@)[0-9.-]+/g, (m, m1) => `${m1}${pkg.version}`)
+
+fs.writeFileSync(path.resolve(__dirname, '..', 'README.md'), md)
